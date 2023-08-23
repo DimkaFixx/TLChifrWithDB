499991b593852db987f452561399e0fc0bfb93b2
dima <dima@DESKTOP-BVK5L5K>
2023-08-23 15:35:20 +0300

    Добавьте файлы проекта.

---
+++ ChifrWithDB/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ChifrWithDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Chifr chifr = new Chifr();  
+                        
+            Console.WriteLine("Вы хотите зашифровать (Y) сообщение или расшифровать (N)?:");
+            string isChipher = Console.ReadLine();
+            Console.WriteLine("На какое кол-во делений смещение по часовой стрелке? (положительное число):");
+            int countbias = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите сообщение:");
+            string message = Console.ReadLine();
+
+            if (isChipher == "Y")
+            {
+                Console.WriteLine($"Ваше сообщение: {chifr.Chifrator(message, countbias)}");
+            }
+            else
+            {
+                Console.WriteLine($"Ваше сообщение: {chifr.DeChifrator(message, countbias)}");
+            }
+        }
+    }
+}
